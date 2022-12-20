16eed800467ae0d0abee157fa3879ab726351a95
prajyot96k <prajyotmane925@gmail.com>
2022-12-20 23:52:48 +0530

    second commit

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -3,5 +3,9 @@ package com.app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //adding some changes
+
+        System.out.print("second commit");
     }
 }
